b30482bd9f65af4c0a5bde8b7a805164ff1fb150
Lucas Mendes <70616909+LucasMABF@users.noreply.github.com>
2024-04-10 19:11:34 -0300

    APC beecrowd
    Melhora na lógica do ex054(1144 - Sequência Lógica).

--- 2024.1/APC/beecrowd/ex054/ex054.c
+++ 2024.1/APC/beecrowd/ex054/ex054.c
@@ -1,33 +1,27 @@
 // 1144 - Sequência Lógica
 #include <stdio.h>
+#include <math.h>
 
 int main(){
-    // 00 01 00 01 00 01 00 01 00 
-    // 01 02 01 04 01 06 01 08 01 
-    // 01 06 01 18 01 36 01 60 01
-    // +12 +18 +24 
     int n;
     scanf("%d", &n);
-    n *= 2;
-    
-    int a = 1;
-    int b = 1;
-    int c = 1;
-    int somador = 0;
-    int somador_somador = 6;
-    for(int i = 0; i < n; i++){
-        printf("%d %d %d\n", a, b, c);
-        if(i % 2 == 0){
-            b += 1;
-            c += 1;
-        }else{
-            a++;
-            b += i + 1;
-            somador += somador_somador;
-            somador_somador += 6;
-            c += somador;
-        }
-    }
+
+    for(int i = 1; i <= n; i++){
+        double a = pow(i, 2);
+        double b = pow(i, 3);
+        printf("%d %.0lf %.0lf\n", i, a, b);
+        printf("%d %.0lf %.0lf\n", i, a + 1, b + 1);
+    }   
 
     return 0;
 }
+
+// 00 01 00 01 00 01 00 01 00 
+// 01 02 01 04 01 06 01 08 01 
+// 01 06 01 18 01 36 01 60 01
+// +12 +18 +24 
+
+// 1 8 27 64 125 216
+//  7 19  37 61 91
+//   12 18 24  30
+//     6  6  6   
